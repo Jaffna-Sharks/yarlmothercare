a1ebfcce377e32eb78ccdca7193d676efa46336d
davisaaron <davisabraham32@gmail.com>
2020-10-13 12:56:50 +0530

    home

--- src/hospitalhomepage/HospitalHomePage.java
+++ src/hospitalhomepage/HospitalHomePage.java
@@ -1,3 +1,6 @@
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,10 +8,7 @@
  */
 package hospitalhomepage;
 
-/**
- *
- * @author kiros
- */
+
 public class HospitalHomePage {
 
     /**
@@ -16,6 +16,9 @@ public class HospitalHomePage {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        LogIn ln =new LogIn();
+        ln.show();
+   
     }
     
 }
